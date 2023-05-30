efbfc6447f6fa9741a114d22e17d6a11cc07ab18
Suresh Kumar <suresh710k@gmail.com>
2023-05-31 00:52:55 +0530

    update

--- CastleDialogue/main.js
+++
@@ -1,5 +0,0 @@
-const button = document.querySelector("button")
-const castleModalWeapons = document.querySelector(".castleModalWeapons")
-button.addEventListener("click", () => {
-    castleModalWeapons.showModal();
-})
